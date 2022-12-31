0160830c08710c781f40b68f33ec28df09ded128
NishantSingh02 <nst7372@gmail.com>
2022-12-31 23:37:51 +0530

    Initial Commit

--- src/Sorting_and_Searching/Apartments/Main.java
+++ src/Sorting_and_Searching/Apartments/Main.java
@@ -1,2 +1,46 @@
-package Sorting_and_Searching.Apartments;public class Main {
+package Sorting_and_Searching.Apartments;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        long k = scan.nextInt();
+
+        Integer[] a = new Integer[n];
+        Integer[] b = new Integer[m];
+        for(int i=0;i<n;i++)
+        {
+            a[i] = scan.nextInt();
+        }
+        for(int i=0;i<m;i++)
+        {
+            b[i] = scan.nextInt();
+        }
+
+        Arrays.sort(a, Collections.reverseOrder());
+        Arrays.sort(b,Collections.reverseOrder());
+
+        int i=0;
+        int j=0;
+        int c =0 ;
+        while(i<n && j<m)
+        {
+            if(b[j]<a[i]+k && b[j]>a[i]-k) {
+                c++;
+                i++;
+                j++;
+            }
+            else if(b[j]<a[i])
+                j++;
+            else
+                i++;
+        }
+        System.out.println(c);
+    }
 }
--- src/Sorting_and_Searching/Distinct_Numbers/Main.java
+++ src/Sorting_and_Searching/Distinct_Numbers/Main.java
@@ -1,8 +1,33 @@
+package Sorting_and_Searching.Distinct_Numbers;
+
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Distinct_Numbers {
+//TLE
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//
+//        int n = scan.nextInt();
+//        int[] a = new int[n];
+//
+//        for(int i=0;i<n;i++)
+//            a[i] = scan.nextInt();
+//        Set<Integer> set = new HashSet<>();
+//
+//        for(int x:a)
+//        {
+//            set.add(x);
+//        }
+//        System.out.println(set.size());
+//    }
+//}
+
+
+//ACCEPTED
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -11,12 +36,14 @@ public class Distinct_Numbers {
 
         for(int i=0;i<n;i++)
             a[i] = scan.nextInt();
-        Set<Integer> set = new HashSet<>();
+        Arrays.sort(a);
 
-        for(int x:a)
+        int c=1;
+        for(int i=0;i<n-1;i++)
         {
-            set.add(x);
+            if(a[i]!=a[i+1])
+                c++;
         }
-        System.out.println(set.size());
+        System.out.println(c);
     }
 }
